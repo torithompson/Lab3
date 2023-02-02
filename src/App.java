55c79418798f0e273f2366cc1e634acf2e7934c0
Tori <tori.thompson@student.sl.on.ca>
2023-02-02 18:18:07 -0500

    update App.class, Player.class, App.java and Player.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Player player1 = new Player("Tori");
+        player1.addSport("soccer");
+        player1.addSport("hockey");
+        player1.addSport("rugby");
+
+        Player player2 = new Player("Chris");
+        player2.addSport("basketball");
+        player2.addSport("baseball");
+        player2.addSport("golf");
+        player2.addSport("rugby");
+
+        Player player3 = new Player("Megan");
+        player3.addSport("soccer");
+        player3.addSport("hockey");
+        player3.addSport("water polo");
+
+        String[] listOfSports = { "soccer", "hockey", "rugby", "basketball", "baseball", "golf", "water polo" };
+
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        players.add(player3);
+
+        for (String sport : listOfSports) {
+            for (Player player : players) {
+                ArrayList<String> sports = player.getSports();
+                if (sports.contains(sport)) {
+                    System.out.println(player.name + " plays " + sport);
+                }
+            }
+        }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,28 @@
 import java.util.ArrayList;
 
 public class Player {
-    private String name;
-    ArrayList<String> sports = new ArrayList<String>();
+    String name;
+    ArrayList<String> sports;
+
+    public Player(String name) {
+        this.name = name;
+        sports = new ArrayList<>();
+    }
+
+    public void addSport(String sport) {
+        sports.add(sport);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getSports() {
+        return sports;
+    }
+
+    @Override
+    public String toString() {
+        return name + " plays " + sports;
+    }
 }
